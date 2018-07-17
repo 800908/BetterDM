bdbf5c98e80b53aabb9f4008ff78520cf2f5c23b
800908 <800908@gmail.com>
2018-07-17 13:27:33 +0430

    make just basic main window

--- mainwinUI.py
+++ mainwinUI.py
@@ -1,8 +1,28 @@
 import sys
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtGui
 
-class MainWindow(QMainWindow):
+class MainWindow(QtGui.QMainWindow):
 
 	def __init__(self, parent= None):
 		super(MainWindow, self).__init__(parent)
-		
+
+		self.initUI()
+
+	def initUI(self):
+		self.setWindowTitle("Better Download Manager")
+		self.setGeometry(50, 50, 640, 480)
+
+
+
+def runApp():
+	app= QtGui.QApplication(sys.argv)
+	
+	main_win= MainWindow()
+	main_win.showMaximized()
+
+	sys.exit(app.exec_())
+
+
+
+if __name__ == "__main__": runApp()
+
