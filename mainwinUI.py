48f95e30906cb204037c736f2c844ffe282b8430
800908 <800908@gmail.com>
2018-07-17 15:41:18 +0430

    fix pep8 warning

--- mainwinUI.py
+++ mainwinUI.py
@@ -1,28 +1,27 @@
 import sys
 from PyQt4 import QtGui
 
+
 class MainWindow(QtGui.QMainWindow):
 
-	def __init__(self, parent= None):
-		super(MainWindow, self).__init__(parent)
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
 
-		self.initUI()
+        self.initUI()
 
-	def initUI(self):
-		self.setWindowTitle("Better Download Manager")
-		self.setGeometry(50, 50, 640, 480)
-
+    def initUI(self):
+        self.setWindowTitle("Better Download Manager")
+        self.setGeometry(50, 50, 640, 480)
 
 
 def runApp():
-	app= QtGui.QApplication(sys.argv)
-	
-	main_win= MainWindow()
-	main_win.showMaximized()
+    app = QtGui.QApplication(sys.argv)
 
-	sys.exit(app.exec_())
+    main_win = MainWindow()
+    main_win.showMaximized()
+
+    sys.exit(app.exec_())
 
 
-
-if __name__ == "__main__": runApp()
-
+if __name__ == "__main__":
+    runApp()
