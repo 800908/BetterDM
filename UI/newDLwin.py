df8e1a8ee01c31be0cd65237456a87c92bd2fa3f
800908 <800908@gmail.com>
2018-07-23 14:51:31 +0430

    Some Items addes to New DL window

--- UI/newDLwin.py
+++ UI/newDLwin.py
@@ -17,29 +17,48 @@ class NewDLDLG(QtGui.QDialog):
         self.setWindowTitle("BDM - New Download")
         self.setMinimumSize(450, 200)
 
+        # ======URL========================================
         lblURL = QtGui.QLabel("File &URL:")
         self.ledtURL = QtGui.QLineEdit("")
         lblURL.setBuddy(self.ledtURL)
         lblURL.setAlignment(QtCore.Qt.AlignRight)
 
+        # ======Mirror=====================================
         lblMirror = QtGui.QLabel("&Mirror:")
         self.ledtMirror = QtGui.QLineEdit("")
         lblMirror.setBuddy(self.ledtMirror)
         lblMirror.setAlignment(QtCore.Qt.AlignRight)
 
+        # ======File Name==================================
         lblFileName = QtGui.QLabel("&File Name:")
         self.ledtFileName = QtGui.QLineEdit("")
         lblFileName.setBuddy(self.ledtFileName)
         lblFileName.setAlignment(QtCore.Qt.AlignRight)
 
+        # ======Folder Name================================
+        lblSaveFolder = QtGui.QLabel("&Save Folder:")
+        self.cbSaveFolder = QtGui.QComboBox()
+        self.tbtnSaveFolder = QtGui.QToolButton()
+        self.cbSaveFolder.setEditable(True)
+        lblSaveFolder.setBuddy(self.cbSaveFolder)
+        lblSaveFolder.setAlignment(QtCore.Qt.AlignRight)
+
+        # ======Layout=====================================
         glayMain = QtGui.QGridLayout()
+        glayMain.setColumnStretch(1, 5)
 
         glayMain.addWidget(lblURL, 0, 0)
-        glayMain.addWidget(self.ledtURL, 0, 1)
+        glayMain.addWidget(self.ledtURL, 0, 1, 1, 2)
+
         glayMain.addWidget(lblMirror, 1, 0)
-        glayMain.addWidget(self.ledtMirror, 1, 1)
+        glayMain.addWidget(self.ledtMirror, 1, 1, 1, 2)
+
         glayMain.addWidget(lblFileName, 2, 0)
-        glayMain.addWidget(self.ledtFileName, 2, 1)
+        glayMain.addWidget(self.ledtFileName, 2, 1, 1, 2)
+
+        glayMain.addWidget(lblSaveFolder, 3, 0)
+        glayMain.addWidget(self.cbSaveFolder, 3, 1)
+        glayMain.addWidget(self.tbtnSaveFolder, 3, 2)
 
         self.setLayout(glayMain)
 
